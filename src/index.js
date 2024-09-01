1a8c34de7c8396ce4f32141355d40e8e127ebd79
Taylor Beck <beck.taylorg@gmail.com>
2024-09-01 00:59:05 -0700

    Update default port

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/chickens', chickenRoutes);
 
 /* SERVER */
-const port = Number(process.env.PORT) || 3001;
+const port = Number(process.env.PORT) || 8080;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
